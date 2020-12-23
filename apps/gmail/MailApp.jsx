9d6df553ed832c2952b3c32d3839e2d7c32ae453
Viktoria Gordeeva <viktoria71311@gmail.com>
2020-12-23 13:25:47 +0200

    keep start vika

--- apps/gmail/MailApp.jsx
+++ apps/gmail/MailApp.jsx
@@ -1,13 +1,58 @@
 import { AppHeader } from '../../cmps/App-header.jsx'
+import { MailService } from '../gmail/services/mail-service.js'
+import { EmailList } from '../gmail/cmps/Email-list.jsx'
 
 
 export class MailApp extends React.Component {
+
+    state = {
+        emails:[],
+        filterBy:{
+            title:''
+        },
+
+    }
+
+    /// INIT
+    componentDidMount(){
+        this.loadEmail()
+
+    }
+
+    loadEmail = () =>{
+        const emails = MailService.query()
+        this.setState({
+            emails
+        })
+        // console.log(emails);
+    }
+
+
+    getEmailsForDisplay = () =>{
+        const {filterBy} = this.state
+        return this.state.emails.filter((email) =>{
+            return email.title.toLowerCase().includes(filterBy.title.toLowerCase())
+        })
+
+    }
+
+
+
+
     render() {
         return (
             <section>
+
                 <AppHeader />
                 MailApp
+                <section>
+                    <EmailList emails={this.getEmailsForDisplay()}/>
+
+
+                </section>
+
             </section>
         )
     }
 }
+
--- apps/keep/KeepApp.jsx
+++ apps/keep/KeepApp.jsx
@@ -1,7 +1,26 @@
 import { AppHeader } from '../../cmps/App-header.jsx'
+import { keepService } from './services/keepService.js'
 
 
 export class KeepApp extends React.Component {
+    state = {
+        notes: null,
+        answers: []
+    }
+
+    componentDidMount() {
+        this.loadNotes()
+        // .then((res)=> this.setState({ answers: new Array(this.state.notes.length) }))      
+        // this.setState({ answers: new Array(this.state.notes.length) }) 
+    }
+
+
+    loadNotes() {
+        keepService.query()
+            .then(notes => this.setState({ notes , answers: new Array(this.state.notes.length) }))
+    }
+
+
     render() {
         return (
             <section>
