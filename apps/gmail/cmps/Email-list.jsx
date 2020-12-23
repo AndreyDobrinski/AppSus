2b363f4bd4e8c53468e6305f8dd24651d4c16687
Andrey <naruto19d@gmail.com>
2020-12-23 15:15:49 +0200

    work on emial

--- apps/gmail/cmps/Email-list.jsx
+++ apps/gmail/cmps/Email-list.jsx
@@ -1,10 +1,12 @@
 
 import { EmailPreview } from '../cmps/Email-preview.jsx'
 
-export function EmailList({emails}){
+export function EmailList({emails , onEmailPreview , onEmailDelete}){
     return (
+
         emails.map(email =>{
-            return <EmailPreview email={email} key={email.id} />
+            return <EmailPreview email={email} key={email.id} onEmailPreview={onEmailPreview} onEmailDelete={onEmailDelete} />
         })
+
     )
 }
---
+++ apps/gmail/cmps/Email-options.jsx
@@ -0,0 +1,9 @@
+export function EmailOptions (){
+
+    return (
+        <div className="email-options-container">
+            <button className="email-compose">Compose</button>
+        </div>
+
+    )
+}
