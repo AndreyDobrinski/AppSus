6359e4485fd57c83461170940eff1b88fd4272b0
Viktoria Gordeeva <viktoria71311@gmail.com>
2020-12-25 22:52:30 +0200

    Keep: clear of search, placeholder, CSS

--- apps/keep/cmps/NoteCreation.jsx
+++ apps/keep/cmps/NoteCreation.jsx
@@ -5,9 +5,8 @@ export class NoteCreation extends React.Component {
             type: "NoteText",
             // isPinned: true,
             info: {}
-        },
-        stop:false
-        
+        }
+
     }
 
     componentDidMount() {
@@ -17,7 +16,7 @@ export class NoteCreation extends React.Component {
     handleChange = (ev) => {
 
         var value = ev.target.value
-        const noteCopy = { ...this.state.note};
+        const noteCopy = { ...this.state.note };
         console.log('this.state.type', this.state.note.type)
 
         if (this.state.note.type === "NoteText") {
@@ -41,27 +40,53 @@ export class NoteCreation extends React.Component {
     onAddNote = (ev) => {
         ev.preventDefault();
         var copy = { ...this.state.note }
-        console.log('in noteCre to parent', this.state)
-        this.setState({note:{type:"NoteText", info: null }},  ()=>{this.props.onAddNote(copy); this.setState({stop: true})})
-       
+        console.log('in noteCre to parent', this.state.note)
+        this.props.onAddNote(copy);
+        this.setState({ note: { type: "NoteText", info: { txt: '', url: '', todos: [''] } } })
         // this.props.onAddNote(this.state);
     }
 
     onChangeNoteType = (ev) => {
-        var noteCopy = {...this.state.note}
+        var noteCopy = { ...this.state.note }
         if (ev.target.name === "NoteText") {
             noteCopy.type = "NoteText"
-            this.setState({note:noteCopy}, ()=>{console.log('Set to TEXT', this.state.note)})
+            this.setState({ note: noteCopy }, () => { console.log('Set to TEXT', this.state.note) })
             // console.log('Set to TEXT')
         }
         if (ev.target.name === "NoteImg") {
             noteCopy.type = "NoteImg"
-            this.setState({note:noteCopy}, ()=>{console.log('Set to IMG', this.state.note)})
+            this.setState({ note: noteCopy }, () => { console.log('Set to IMG', this.state.note) })
             console.log('Set to IMG')
         }
         if (ev.target.name === "NoteTodos") {
             noteCopy.type = "NoteTodos"
-            this.setState({note:noteCopy})
+            this.setState({ note: noteCopy })
+        }
+    }
+
+    setValue = () => {
+        if (this.state.note.type === "NoteText") {
+            return this.state.note.info.txt
+        }
+        if (this.state.note.type === "NoteImg") {
+            return this.state.note.info.url
+        }
+        if (this.state.note.type === "NoteTodos") {
+            if (!this.state.note.info.todos) return ''
+            var txts = this.state.note.info.todos.map(el => el.txt)
+            var res = txts.join(',')
+            return res
+        }
+    }
+    setPlaceHolder = () => {
+        if (this.state.note.type === "NoteText") {
+            return "What's on your mind..."
+        }
+        if (this.state.note.type === "NoteImg") {
+            return "Enter image URL..."
+        }
+        if (this.state.note.type === "NoteTodos") {
+            return "Enter comma separated list..."
         }
     }
 
@@ -71,15 +96,15 @@ export class NoteCreation extends React.Component {
             <section className="note-creator">
                 <form onSubmit={this.onAddNote}>
                     <input type="text" name="txt" className="note-creator-inp"
-                        // value={this.state.note.info.txt && this.state.note.info.url && this.state.noteinfo.todos.join(',')}
-                        placeholder="Whats on your mind..."
-                        onChange={this.state.stop?()=>{console.log('ppppp')}:this.handleChange} />
+                        value={this.setValue()}
+                        placeholder={this.setPlaceHolder()}
+                        onChange={this.handleChange} />
                     <button type="submit" hidden></button>
                     {/* <button name="Save" className="fas create" onClick={this.onAddNote}></button> */}
                 </form>
-                    <button name="NoteText" className="fas text" onClick={this.onChangeNoteType}></button>
-                    <button name="NoteImg" className="far img" onClick={this.onChangeNoteType}></button>
-                    <button name="NoteTodos" className="fas todo" onClick={this.onChangeNoteType}></button>
+                <button name="NoteText" className="fas text" onClick={this.onChangeNoteType}></button>
+                <button name="NoteImg" className="far img" onClick={this.onChangeNoteType}></button>
+                <button name="NoteTodos" className="fas todo" onClick={this.onChangeNoteType}></button>
             </section>
         )
     }
